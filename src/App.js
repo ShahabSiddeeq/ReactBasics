081036b1eeb02bb27511c6a0cc5f830d6a56dfe8
Shahab Siddeeq <shahabsiddeeq1@gmail.com>
2022-05-27 16:05:36 +0500

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
   constructor(){
     super();
     this.state={
-      monsters:[
-        {
-          name: "Frankenstein",
-          id: 'asc1'
-      },
-        {
-          name: "Dracula",
-          id:'ar1c'
-        },
-        {
-          name: "Zombie",
-          id:'ars1'
-        }
-      ]
+      monsters:[]
+        
     };
-
-  }
+}
+componentDidMount(){
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then( response => response.json())
+      .then(users => this.setState({monsters:users}));
+}
   render(){
     return(
     <div className="App">
